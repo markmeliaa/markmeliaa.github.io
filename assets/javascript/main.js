103f8d0043522759625a9f53275474561555a579
Marc Alcón Meliá <82601780+markmeliaa@users.noreply.github.com>
2023-03-14 20:48:48 +0100

    New custom cursor

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -270,7 +270,46 @@
     }
   }
 
-  // Lock screen orientation to portrait
-  window.screen.orientation.lock('portrait');
+  // Cursor for computer
+  function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+  }
+    
+  if (!isMobile()) {
+    const $bigBall = document.querySelector('.cursor__ball--big');
+    const $smallBall = document.querySelector('.cursor__ball--small');
+    const $hoverables = document.querySelectorAll('.hoverable');
+
+    // Listeners
+    document.body.addEventListener('mousemove', onMouseMove);
+    for (let i = 0; i < $hoverables.length; i++) {
+      $hoverables[i].addEventListener('mouseenter', onMouseHover);
+      $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+    }
+
+    // Move the cursor
+    function onMouseMove(e) {
+      TweenMax.to($bigBall, .4, {
+        x: e.pageX - 15,
+        y: e.pageY - 15
+      })
+      TweenMax.to($smallBall, .1, {
+        x: e.pageX - 5,
+        y: e.pageY - 7
+      })
+    }
+
+    // Hover an element
+    function onMouseHover() {
+      TweenMax.to($bigBall, .3, {
+        scale: 3
+      })
+    }
+    function onMouseHoverOut() {
+      TweenMax.to($bigBall, .3, {
+        scale: 1
+      })
+    } 
+  }
 
 })()
