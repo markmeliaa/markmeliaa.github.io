8af9a02909d8eb1eb562530630d5c71c4e595795
Marc Alcón Meliá <82601780+markmeliaa@users.noreply.github.com>
2023-03-16 21:25:14 +0100

    About final version (this time for sure) and start traits section

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -306,15 +306,19 @@
         scale: 3,
       })
     }
+
     function onMouseHoverOut() {
       TweenMax.to($bigBall, .3, {
         scale: 1
       })
     }
+
+    // Scroll to the top of the page when refreshing
+    /* TODOMine: Uncomment this function once finished
+    window.onload = function () {
+    window.scrollTo(0,0);
+    };
+    */
   }
 
-  // Scroll to the top of the page when refreshing
-  window.onload = function () {
-    window.scrollTo(0,0);
-  };
 })()
