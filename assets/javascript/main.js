483a939047b56aab194bd89b53205663f0274573
Marc Alcón Meliá <82601780+markmeliaa@users.noreply.github.com>
2023-03-21 20:49:24 +0100

    Portfolio cells set up and basic hover effect with mouse

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -321,4 +321,20 @@
     */
   }
 
+  // Mouse hover light efect over the portfolio work
+  const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+  }
+
+  for(const card of document.querySelectorAll("#work-card")) {
+    card.onmousemove = e => handleOnMouseMove(e);
+  }
+
 })()
